a9e1055929d66a51d62df181d3ba3ef5454c5089
Avi Tuchband <avitoohband@gmail.com>
2023-11-12 17:51:07 +0200

    first commit

---
+++ src/test/java/com/avituchband/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avituchband.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
